d95d429e804b385159a1240e6370f5e047398bed
dpatel210 <dishantpatel421@gmail.com>
2022-11-05 13:26:34 -0400

    Initial Commit

---
+++ FindYourDrink/Model/Drink.swift
@@ -0,0 +1,18 @@
+//
+//  Drink.swift
+//  FindYourDrink
+//
+//  Created by Dishant  patel on 11/5/22.
+//
+
+import Foundation
+
+struct Drink: Codable {
+    let idDrink: String
+    let strDrink: String
+    let strTags: String
+    let strInstructions: String
+    let 
+    
+    
+}
---
+++ FindYourDrink/Model/DrinksList.swift
@@ -0,0 +1,13 @@
+//
+//  DrinksList.swift
+//  FindYourDrink
+//
+//  Created by Dishant  patel on 11/5/22.
+//
+
+import Foundation
+
+
+struct DrinksList {
+    
+}
