c76534e5a9835c590fb0b4977f6a16fe2952829a
dpatel210 <dishantpatel421@gmail.com>
2022-11-05 20:55:30 -0400

    Fixed to use JSON file

--- FindYourDrink/ContentView.swift
+++ FindYourDrink/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
         NavigationView {
             DrinksMainView()
         }
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
--- FindYourDrink/Network/Network.swift
+++ FindYourDrink/Network/Network.swift
@@ -14,13 +14,15 @@ class Network {
     let session = URLSession(configuration: .default)
     
     func getRandomDrinks(completion: @escaping (Result<DrinksList, Error>) -> ()) {
-        let url = URL(string: "https://www.thecocktaildb.com/api/json/v1/1/random.php")
+//        let url = URL(string: "https://www.thecocktaildb.com/api/json/v1/1/random.php")
+//
+//        guard let url = url else {
+//            print("Failure due to bad url")
+//            return
+//
+//        }
         
-        guard let url = url else {
-            print("Failure due to bad url")
-            return
-            
-        }
+        guard let url = Bundle.main.url(forResource: "MockData1", withExtension: "json") else { return }
         
         let dataTask = session.dataTask(with: url) { data, response, error in
             
